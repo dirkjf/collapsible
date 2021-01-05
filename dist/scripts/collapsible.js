1b1d95b7d88ca59f53fd2568d6304c3d81aa9086
dirkjf <d.j.faber@outlook.com>
2021-01-05 17:30:38 +0100

    add dist files

---
+++ dist/scripts/collapsible.js
@@ -0,0 +1,54 @@
+/******/ (function() { // webpackBootstrap
+!function() {
+document.addEventListener('click', function (event) {
+  var element = event.target;
+  var targetSelector = element.getAttribute('data-collapsible-target');
+
+  if (targetSelector !== null) {
+    toggleCollapse(targetSelector);
+  }
+}, false);
+
+var toggleCollapse = function toggleCollapse(targetSelector) {
+  var targets = document.querySelectorAll('.' + targetSelector);
+  targets.forEach(function (target) {
+    if (!target.classList.contains('collapsible-show')) {
+      target.style.maxHeight = target.scrollHeight + 'px';
+    } else {
+      target.style.maxHeight = 0 + 'px';
+    }
+
+    target.classList.toggle('collapsible-show');
+  });
+};
+
+var closeOnClickOutsideElements = document.querySelectorAll('[data-close-on-click-outside]');
+closeOnClickOutsideElements.forEach(function (element) {
+  return hideOnClickOutside(element);
+});
+
+function hideOnClickOutside(element) {
+  var outsideClickListener = function outsideClickListener(event) {
+    // Third checks if the element clicked isn't the toggler, which would result in a double click event that conflict each other.
+    if (!element.contains(event.target) && isVisible(element) && !element.classList.contains(event.target.getAttribute('data-collapsible-target'))) {
+      element.classList.remove('collapsible-show');
+      element.style.maxHeight = 0;
+    }
+  };
+
+  document.addEventListener('click', outsideClickListener);
+
+  var isVisible = function isVisible(elem) {
+    return !!elem && !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length);
+  }; // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
+
+}
+}();
+!function() {
+"use strict";
+// extracted by mini-css-extract-plugin
+
+}();
+/******/ })()
+;
+//# sourceMappingURL=collapsible.js.map
