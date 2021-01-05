343235aaded27272371a4a3c5a9412d19469f50b
dirkjf <d.j.faber@outlook.com>
2021-01-05 18:55:54 +0100

    fix: load when document is ready

--- dist/scripts/collapsible.js
+++ dist/scripts/collapsible.js
@@ -1,48 +1,62 @@
 /******/ (function() { // webpackBootstrap
 !function() {
-document.addEventListener('click', function (event) {
-  var element = event.target;
-  var targetSelector = element.getAttribute('data-collapsible-target');
+function collapsible() {
+  document.addEventListener('click', function (event) {
+    var element = event.target;
+    var targetSelector = element.getAttribute('data-collapsible-target');
 
-  if (targetSelector !== null) {
-    toggleCollapse(targetSelector);
-  }
-}, false);
-
-var toggleCollapse = function toggleCollapse(targetSelector) {
-  var targets = document.querySelectorAll('.' + targetSelector);
-  targets.forEach(function (target) {
-    if (!target.classList.contains('collapsible-show')) {
-      target.style.maxHeight = target.scrollHeight + 'px';
-    } else {
-      target.style.maxHeight = 0 + 'px';
+    if (targetSelector !== null) {
+      toggleCollapse(targetSelector);
     }
+  }, false);
 
-    target.classList.toggle('collapsible-show');
-  });
-};
+  var toggleCollapse = function toggleCollapse(targetSelector) {
+    var targets = document.querySelectorAll('.' + targetSelector);
+    targets.forEach(function (target) {
+      if (!target.classList.contains('collapsible-show')) {
+        target.style.maxHeight = target.scrollHeight + 'px';
+      } else {
+        target.style.maxHeight = 0 + 'px';
+      }
 
-var closeOnClickOutsideElements = document.querySelectorAll('[data-close-on-click-outside]');
-closeOnClickOutsideElements.forEach(function (element) {
-  return hideOnClickOutside(element);
-});
-
-function hideOnClickOutside(element) {
-  var outsideClickListener = function outsideClickListener(event) {
-    // Third checks if the element clicked isn't the toggler, which would result in a double click event that conflict each other.
-    if (!element.contains(event.target) && isVisible(element) && !element.classList.contains(event.target.getAttribute('data-collapsible-target'))) {
-      element.classList.remove('collapsible-show');
-      element.style.maxHeight = 0;
-    }
+      target.classList.toggle('collapsible-show');
+    });
   };
 
-  document.addEventListener('click', outsideClickListener);
+  var closeOnClickOutsideElements = document.querySelectorAll('[data-close-on-click-outside]');
+  closeOnClickOutsideElements.forEach(function (element) {
+    return hideOnClickOutside(element);
+  });
 
-  var isVisible = function isVisible(elem) {
-    return !!elem && !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length);
-  }; // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
+  function hideOnClickOutside(element) {
+    var outsideClickListener = function outsideClickListener(event) {
+      // Third checks if the element clicked isn't the toggler, which would result in a double click event that conflict each other.
+      if (!element.contains(event.target) && isVisible(element) && !element.classList.contains(event.target.getAttribute('data-collapsible-target'))) {
+        element.classList.remove('collapsible-show');
+        element.style.maxHeight = 0;
+      }
+    };
 
+    document.addEventListener('click', outsideClickListener);
+
+    var isVisible = function isVisible(elem) {
+      return !!elem && !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length);
+    }; // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
+
+  }
 }
+
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+    // call on next available tick
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+docReady(docReady);
 }();
 !function() {
 "use strict";
--- src/scripts/collapsible.js
+++ src/scripts/collapsible.js
@@ -1,36 +1,50 @@
-document.addEventListener('click', (event) => {
+function collapsible(){
+  document.addEventListener('click', (event) => {
 
-  const element = event.target;
-  const targetSelector = element.getAttribute('data-collapsible-target');
+    const element = event.target;
+    const targetSelector = element.getAttribute('data-collapsible-target');
 
-  if (targetSelector !== null) {
-    toggleCollapse(targetSelector);
-  }
-}, false);
-
-const toggleCollapse = targetSelector => {
-  const targets = document.querySelectorAll('.' + targetSelector);
-  targets.forEach(target => {
-    if (!target.classList.contains('collapsible-show')) {
-      target.style.maxHeight = target.scrollHeight + 'px';
-    } else {
-      target.style.maxHeight = 0 + 'px';
+    if (targetSelector !== null) {
+      toggleCollapse(targetSelector);
     }
-    target.classList.toggle('collapsible-show');
-  });
+  }, false);
+
+  const toggleCollapse = targetSelector => {
+    const targets = document.querySelectorAll('.' + targetSelector);
+    targets.forEach(target => {
+      if (!target.classList.contains('collapsible-show')) {
+        target.style.maxHeight = target.scrollHeight + 'px';
+      } else {
+        target.style.maxHeight = 0 + 'px';
+      }
+      target.classList.toggle('collapsible-show');
+    });
+  }
+
+  const closeOnClickOutsideElements = document.querySelectorAll('[data-close-on-click-outside]');
+  closeOnClickOutsideElements.forEach(element => hideOnClickOutside(element))
+
+  function hideOnClickOutside(element) {
+    const outsideClickListener = event => {
+      // Third checks if the element clicked isn't the toggler, which would result in a double click event that conflict each other.
+      if (!element.contains(event.target) && isVisible(element) && !element.classList.contains(event.target.getAttribute('data-collapsible-target'))) {
+        element.classList.remove('collapsible-show')
+        element.style.maxHeight = 0;
+      }
+    }
+    document.addEventListener('click', outsideClickListener)
+    const isVisible = elem => !!elem && !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length) // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
+  }
 }
 
-const closeOnClickOutsideElements = document.querySelectorAll('[data-close-on-click-outside]');
-closeOnClickOutsideElements.forEach(element => hideOnClickOutside(element))
-
-function hideOnClickOutside(element) {
-  const outsideClickListener = event => {
-    // Third checks if the element clicked isn't the toggler, which would result in a double click event that conflict each other.
-    if (!element.contains(event.target) && isVisible(element) && !element.classList.contains(event.target.getAttribute('data-collapsible-target'))) {
-      element.classList.remove('collapsible-show')
-      element.style.maxHeight = 0;
-    }
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+    // call on next available tick
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
   }
-  document.addEventListener('click', outsideClickListener)
-  const isVisible = elem => !!elem && !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length) // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
 }
+
+docReady(docReady);
